02fbd5479c613ae913b6c86d18cc580888c92844
Rogerpervaz <ulisescodi@gmail.com>
2023-10-30 17:54:36 -0600

    ForgotPassword Screen
    Completed.(?)

--- src/screens/Auth/ForgotPassword/forgotPassword.tsx
+++ src/screens/Auth/ForgotPassword/forgotPassword.tsx
@@ -1,22 +1,82 @@
-import React, {useEffect} from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Image,
+  TextInput,
+  Pressable,
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
 import styles from './forgotPasswordStyle';
 import {routerProps} from 'router/RootStackParams';
+import logo from 'images/LogoTec.png';
+import {colors} from 'colors';
+import ButtonComponent from 'components/button/button';
+import forgotPasswordStyle from './forgotPasswordStyle';
+
 function ForgotPasswordScreen({navigation}: routerProps<'ForgotPassword'>) {
+  const [text, setText] = useState(''); // Estado para el valor del campo de entrada
+
+
+  const Actionbutton = () => {
+    navigation.navigate('Login');
+  };
+
   useEffect(() => {
     console.log('Hola2');
     return () => {
-      console.log('RETUNR');
+      console.log('RETURN');
     };
   }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Register Screen</Text>
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate('Login')}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerHeader}>
+        <Pressable onPress={() => navigation.goBack()}>
+          <Text style={styles.textBack}>Regresar</Text>
+        </Pressable>
+        <Image style={styles.imageHeader} resizeMode="contain" source={logo} />
+      </View>
+
+      <View>
+        <Text
+          style={{
+            color: colors.titleText,
+            fontSize: 24,
+            fontWeight: '500',
+            marginTop: 40,
+            marginBottom: 10,
+          }}>
+          Recuperar contraseña
+        </Text>
+        <Text style={{fontSize: 14, fontWeight: '400'}}>
+          Ingresa tu correo electronico con el que te registraste y enviaremos
+          un mail con las instrucciones para recuperar tu contraseña
+        </Text>
+
+        <View style={{alignContent: 'center', marginTop: 100, padding: 10}}>
+          <TextInput
+            style={forgotPasswordStyle.textInput}
+            placeholder="Correo electronico"
+            value={text}
+            onChangeText={newText => setText(newText)} // Actualiza el estado 'text' al escribir
+          />
+
+          <ButtonComponent
+            onPress={() => Actionbutton()}
+            styleButton={forgotPasswordStyle.buttonStyle}
+            buttonText={'Recuperar contraseña'}
+            styleText={forgotPasswordStyle.buttonText}
+          />
+        </View>
+      </View>
+      <View style={forgotPasswordStyle.footScreenContainer}>
+        <Text
+          style={forgotPasswordStyle.proyectitoFootPage}>
+          ProyectITO
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
--- src/screens/Auth/ForgotPassword/forgotPasswordStyle.tsx
+++ src/screens/Auth/ForgotPassword/forgotPasswordStyle.tsx
@@ -1,13 +1,53 @@
 import {colors} from 'colors';
 import {StyleSheet} from 'react-native';
 import {normalize} from 'utils/normalize';
+
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    marginHorizontal: normalize(20),
+  },
+  containerHeader: {
+    flexDirection: 'row',
     alignContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    paddingHorizontal: normalize(30),
-    backgroundColor: colors.white,
+  },
+  textBack: {
+    fontSize: normalize(14),
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: normalize(28),
+  },
+  imageHeader: {
+    width: normalize(42),
+    height: normalize(41),
+  },
+  textInput: {
+    backgroundColor: colors.neutral05,
+    color: colors.neutral60,
+    fontSize: 16,
+    fontWeight: '400',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: colors.primary,
+    borderRadius: 10,
+    height: 50,
+    margin: 10,
+  },
+  buttonText: {fontSize: 16, fontWeight: '600'},
+  footScreenContainer: {
+    marginTop: 'auto',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  proyectitoFootPage: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: colors.titleText,
   },
 });
